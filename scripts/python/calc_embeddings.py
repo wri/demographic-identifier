d14cbfc3818b6e33f2ceb5667f3f17f644ee59c0
John Brandt <john.brandt@yale.edu>
2018-11-26 16:37:45 -0500

    finalize calc_embeddings.py

--- scripts/python/calc_embeddings.py
+++ scripts/python/calc_embeddings.py
@@ -5,7 +5,7 @@ data_en = "../../data/muse/en/tweets_en.txt"
 embedding_en = "../../data/muse/wiki.multi.en.vec"
 
 embeddings_index = {}
-
+print("### Opening embeddings index ###\n")
 f = open(embedding_en)
 for i, line in enumerate(f):
 	values = line.strip().split()
@@ -15,13 +15,24 @@ for i, line in enumerate(f):
 		embeddings_index[word] = coefs
 f.close()
 
+print("### Opening tweets ### \n")
 tweets = []
 for line in open(data_en):
 	tweets.append(line)
 
+print("### Calculating sentence embeddings ### \n")
 sent_embeddings = []
 for i, tweet in enumerate(tweets):
 	twt = tweets[i].split()
 	embs = []
+	sent_emb = np.zeros(300)
 	for i in twt:
-		emb.append(embeddings_index.get(i))
+		if embeddings_index.get(i) is not None:
+			embs.append(embeddings_index.get(i))
+			sent_emb += embeddings_index.get(i)
+	for i in sent_emb:
+		sent_emb[i] /= len(twt)
+	sent_embeddings.append(sent_emb)
+
+print("### Saving normalized sentence embeddings ### \n")
+np.savetxt("../../data/processed/sent_embeddings.txt", sent_embeddings)
