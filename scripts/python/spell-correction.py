c53255e49b2797c5665d262219085bcfbb9a6a98
John Brandt <john.brandt@yale.edu>
2018-12-14 09:56:15 -0500

    fix issues 1 & 2 - line breaks, NA tweets, short tweets

--- scripts/python/spell-correction.py
+++ scripts/python/spell-correction.py
@@ -7,10 +7,32 @@ import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
+
 def ws_tokenizer(text):
     return text.split()
 
-data_en = "../../data/muse/en/tweets_en.txt"
+data_en = "../../data/processed/english_text.txt"
 print("### Opening tweets ### \n")
 tweets = []
 for line in open(data_en):
@@ -36,13 +58,15 @@ text_processor = TextPreProcessor(
     dicts=[emoticons]
 )
 
+printProgressBar(0, len(tweets), prefix = "Progress:", suffix = "Complete", length = 50)
 prepr = []
-for s in tweets:
-    sent = ((" ".join(text_processor.pre_process_doc(s))))
+for s, twt in enumerate(tweets):
+    sent = ((" ".join(text_processor.pre_process_doc(twt))))
     if len(sent.split()) > 0:
         if "…" in sent.split().pop() :
             sentsplit = sent.split()[:-2]
             sent = (" ".join(sentsplit))
+    printProgressBar(s + 1, len(tweets), prefix = 'Progress:', suffix = 'Complete', length = 50)
     prepr.append(sent)
 
 print(len(prepr))
